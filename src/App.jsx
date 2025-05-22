9669c9c4966b3aadcdbd00abc6cd1f4a625edd90
reza rafiyi <dr.rezaxrafiee@gmail.com>
2025-05-22 10:25:38 +0330

    demo1.1parbazai
    نسخه1

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import TestForm from "./pages/TestForm";
+import TestResult from "./pages/TestResult";
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Welcome />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/test' element={<TestForm />} />
+        <Route path='/result' element={<TestResult />} />
+      </Routes>
+    </Router>
+  );
+}
