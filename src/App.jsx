0e45a7b723ddcde06a941321796062bf36019039
reza rafiyi <dr.rezaxrafiee@gmail.com>
2025-05-23 03:07:58 +0330

    imports App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import [React from] "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import TestForm from "./pages/TestForm";
 import TestResult from "./pages/TestResult";
+
 
 export default function App() {
   return (
